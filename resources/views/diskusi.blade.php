51a582a034d1a1b912e62f0ee289946208fe4c25
Reignleiv <rizalp021@gmail.com>
2023-02-12 15:40:04 +0700

    komentar

--- resources/views/diskusi.blade.php
+++ resources/views/diskusi.blade.php
@@ -5,11 +5,12 @@
 
     @foreach ($diskusis as $diskusi)
         <article class="mb-5 border-bottom pb-3">
-            <h3><a href="/diskusi/{{ $diskusi->slug }}" class="text-decoration-none">{{ $diskusi->title }}</a></h3>
-            <p>By. <a href="/user/{{ $diskusi->user->username }}" class="text-decoration-none">{{ $diskusi->user->name }}</a></p>            
+            <h3><a href="/dashboard/diskusi/{{ $diskusi->id }}" class="text-decoration-none">{{ $diskusi->title }}</a></h3>
+            <p>By. <a href="/user/{{ $diskusi->user->username }}" class="text-decoration-none">{{ $diskusi->user->name }}</a>
+            </p>
             <p>{{ $diskusi->excerpt }}</p>
-            <a href="/diskusi/{{ $diskusi->slug }}" class="text-decoration-none">Read more</a>
+            <a href="/dashboard/diskusi/{{ $diskusi->id }}" class="text-decoration-none">Read more</a>
+
         </article>
     @endforeach
-
 @endsection
